4af5ef3ac0f9e580c138cf46397abdfe6901f818
Jonathan Mirabal <jonathan.a.mirabal@gmail.com>
2026-01-04 19:07:00 -0500

    vercel modifications

--- obel-frontend/src/pages/StockDetailPage.js
+++ obel-frontend/src/pages/StockDetailPage.js
@@ -93,7 +93,7 @@ const StockDetailPage = () => {
             setError("");
             try {
                 const res = await fetch(
-                    `http://localhost:5000/api/stocks/detail/${symbol}`
+                    `/api/stocks/detail/${symbol}`
                 );
                 if (!res.ok) throw new Error(`HTTP ${res.status}`);
                 const json = await res.json();
